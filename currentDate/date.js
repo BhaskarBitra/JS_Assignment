f588644e0a30b7e6a5dce9286b29a63fced2cc76
BhaskarBitra <68264821+BhaskarBitra@users.noreply.github.com>
2020-07-14 12:41:32 +0530

    Add files via upload

---
+++ currentDate/date.js
@@ -0,0 +1,14 @@
+var curday = function(sp){
+    today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    
+    if(dd<10) dd='0'+dd;
+    if(mm<10) mm='0'+mm;
+    return (dd+sp+mm+sp+yyyy);
+    };
+    document.write(curday('/'));
+    document.write("\n");
+ 
+    document.write(curday('-'));
